88da53c2bf57748765e0d1e4557de1e30399976f
Sander Demeester <demeester.sander@gmail.com>
2011-08-31 10:52:56 +0200

    Sudokugrid file reads input file

--- sudokusolver/src/modellen/SudokuGrid.java
+++ sudokusolver/src/modellen/SudokuGrid.java
@@ -1,9 +1,10 @@
 package modellen;
 
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
 import java.io.File;
-
-import eventHandlers.EventManager;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class SudokuGrid extends Model{
@@ -13,8 +14,24 @@ public class SudokuGrid extends Model{
 		
 	}
 	
+	private void parsefile(){
+		
+		try{
+			BufferedReader bufferedreader = new BufferedReader(new FileReader(sudokufile));
+			String str;
+			while((str=bufferedreader.readLine()) != null){
+				System.out.println(str);
+			}
+		}catch(IOException ex){
+			System.out.println("Fout gebeurdt tijdens het verwerken van de sudokufile");
+			ex.printStackTrace();
+		}
+		
+	}
+	
 	public void addFile(File sudokufile){
 		this.sudokufile = sudokufile;
+		parsefile();
 		firestateChaned();
 	}
 	@Override
